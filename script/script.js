21524b086c0ad986cb7e29da8bb5649aeb8aff0b
Artyom <theshadhom@gmail.com>
2022-02-08 18:28:47 +0800

    movieDB

--- script/script.js
+++ script/script.js
@@ -8,7 +8,26 @@
 // document.write(arr);
 // console.log(typeof arr[2]);
 
-const category = 'video';
-const obg = 5;
-console.log(`https://someUrl.com/${category}/${obg}`);
+// const category = 'video';
+// const obg = 5;
+// console.log(`https://someUrl.com/${category}/${obg}`);
 
+const numberOfFilm = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMouvieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Какой последний фильм посмотрели?', ''),
+    b = prompt('Какой последний фильм посмотрели?', ''),
+    c = +prompt('Как его оцените?', ''),
+    d = +prompt('Как его оцените?', '');
+
+personalMouvieDB.movies[a] = c;
+personalMouvieDB.movies[b] = d;
+
+console.log(personalMouvieDB);
