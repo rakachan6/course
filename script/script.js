6110cea828020877ee53b31d07a05cb9c48817de
Artyom <theshadhom@gmail.com>
2022-02-08 15:04:14 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// const arr = [];
+// arr[0] = prompt('Ваше имя', '');
+// arr[1] = prompt('Ваша фамилия', '');
+// arr[2] = +prompt('Ваш возраст', '');
+// document.write(arr);
+// console.log(typeof arr[2]);
+
+const category = 'video';
+const obg = 5;
+console.log(`https://someUrl.com/${category}/${obg}`);
+
